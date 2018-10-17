a75c109c4de78c8f2c99459d91e310cf854a85f4
Akil <achsvgusaeng@yahoo.com>
2018-10-17 20:48:20 +0000

    refactor

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,15 +1,19 @@
 import React from 'react';
+import TodoItem from './TodoItem';
 
 const TodoList = (props)=>{
     return(
         <ul>
           {props.todos.map((todo, index)=>{
-            return (<li key={todo.title}>
-              <input onChange={(event)=>props.toggleTodoDone(event, index)} type='checkbox' checked={todo.done}/>
-              {/*<span style={{textDecoration: todo.done ? 'line-through':'inherit'}}>{todo.title}</span>*/}
-              <span className={todo.done ?'done': ''}>{todo.title}</span>
-              <button onClick={()=>{props.removeTodo(index)}}>Remove</button>
-            </li>);
+            return (
+                <TodoItem
+                key={index}
+                index={index}
+                todo={todo}
+                toggleTodoDone={props.toggleTodoDone}
+                removeTodo={props.removeTodo}
+            />
+            );
           })}
         </ul>    
     );
