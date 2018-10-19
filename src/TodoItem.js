46b8d2681e60de4cb22bb0972e535aa9fa8ee355
Akil <achsvgusaeng@yahoo.com>
2018-10-19 21:01:54 +0000

    clean up

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -7,7 +7,7 @@ const TodoItem = (props)=>{
               <input onChange={(event)=>props.toggleTodoDone(event, index)} type='checkbox' checked={todo.done}/>
               {/*<span style={{textDecoration: todo.done ? 'line-through':'inherit'}}>{todo.title}</span>*/}
               <span className={todo.done ?'done': ''}>{todo.title}</span>
-              <button onClick={()=>{props.removeTodo(index)}}>Remove</button>
+              <button onClick={() => {props.removeTodo(index)}}>Remove</button>
         </li>
     );
 };
